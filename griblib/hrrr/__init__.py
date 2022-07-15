7b5dc58e342c5b55e4fa3e794ca2d1a38c95ee9d
Jason Leaver <leaver2000@gmail.com>
2022-07-15 19:39:57 +0000

    formatted with black

--- griblib/hrrr/__init__.py
+++ griblib/hrrr/__init__.py
@@ -1,3 +1,3 @@
 """classes, functions, and objects used for working with HRRR data"""
-__all__ = ["HRRR","load_hrrr"]
+__all__ = ["HRRR", "load_hrrr"]
 from griblib.hrrr.core import HRRR, load_hrrr
--- griblib/hrrr/_zarr.py
+++ griblib/hrrr/_zarr.py
@@ -2,12 +2,17 @@ import json
 import pandas as pd
 from pathlib import Path
 from os.path import dirname
-idx:slice = pd.IndexSlice
+
+idx: slice = pd.IndexSlice
 _table_path = Path(dirname(__file__)).absolute() / "table.json"
-def pythonic_long_name(df:pd.DataFrame)->pd.DataFrame:
-    df["parameter_long_name"] = df["parameter_long_name"].str.replace(" ","_").str.lower()
-    
+
+
+def pythonic_long_name(df: pd.DataFrame) -> pd.DataFrame:
+    df["parameter_long_name"] = df["parameter_long_name"].str.replace(" ", "_").str.lower()
+
     return df
+
+
 with open(_table_path, "rt") as f:
     ZArrTable: pd.DataFrame = (
         pd.DataFrame(**json.load(f))
