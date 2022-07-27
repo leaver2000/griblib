81dfa5a35fb7c2dec5b74d2e5dc32772feae82cb
Jason Leaver <leaver2000@gmail.com>
2022-07-27 19:15:50 +0000

    .

--- griblib/__init__.py
+++
@@ -1,3 +0,0 @@
-"""a library for working with grib data"""
-__all__ = ["__version__"]
-from griblib._version import version as __version__
--- griblib/mrms/__init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ["mmmpy"]
-
-from griblib.mrms import mmmpy
